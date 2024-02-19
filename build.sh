4f145371ef4a122606b619e14f664119d9c48cc9
cofeek-codes <kormyshev11@mail.ru>
2024-02-19 23:21:29 +0500

    seems like you don't need to copy assets

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 set -xe
 
 mkdir -p build
-cp -r -v --update src/assets/ build/
-emcc -Wall -Wextra src/main.c -o build/index.html -I./lib/include -L./lib/lib -lraylib -lm -s USE_GLFW=3 -s ASYNCIFY --shell-file ./template/template.html --preload-file build/assets@assets
+emcc -Wall -Wextra src/main.c -o build/index.html -I./lib/include -L./lib/lib -lraylib -lm -s USE_GLFW=3 -s ASYNCIFY --shell-file ./template/template.html --preload-file src/assets@assets
 
