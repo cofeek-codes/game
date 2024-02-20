b4407f4f0ed464e4716d8e56e4244075c2b636f4
cofeek-codes <kormyshev11@mail.ru>
2024-02-21 00:38:44 +0500

    tiles placed

--- src/main.c
+++ src/main.c
@@ -1,25 +1,65 @@
 #include "raylib.h"
 
-int main(void) {
-  int screenWidth = 1920;
-  int screenHeight = 1080;
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
 
-  InitWindow(screenWidth, screenHeight, "survivalist");
+// game constants
+
+#define GROUND_HEIGHT 3
+
+// game functions
+
+void DrawRockCol(Texture2D texture, int x) {
+  for (int i = 0; i < GROUND_HEIGHT + 1; ++i) {
+    DrawTexture(texture, (x * texture.width),
+                GetScreenHeight() - (texture.height * i), WHITE);
+  }
+}
+
+int main(void) {
+
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "survivalist");
+
+  // load resources
 
   Texture2D background_texture =
       LoadTexture("assets/background/background_1.png");
+  Texture2D rock_tile_texture = LoadTexture("assets/tiles/rock_tile.png");
+  Texture2D grass_tile_texture = LoadTexture("assets/tiles/grass_tile.png");
+  // load resources
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    // Draw the texture stretched to fullscreen
+    // background texture stretched to fullscreen
     DrawTexturePro(
         background_texture,
         (Rectangle){0, 0, (float)background_texture.width,
                     (float)-background_texture.height},
         (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
         (Vector2){0, 0}, 0.0f, WHITE);
+    // background texture stretched to fullscreen
+
+    // rock tiles
+    // vertical loop
+
+    for (int i = 0; i < (GetScreenWidth() / rock_tile_texture.width) + 1; i++) {
+      DrawRockCol(rock_tile_texture, i);
+    }
+    // rock tiles
+
+    // grass_tiles
+
+    /* DrawTexture(grass_tile_texture, GetScreenWidth() / 2, GetScreenHeight() /
+     * 2, */
+    /*             WHITE); */
+
+    DrawTexture(grass_tile_texture, 0,
+                GetScreenHeight() -
+                    ((GROUND_HEIGHT + 1) * grass_tile_texture.height),
+                WHITE);
+    // grass_tiles
 
     EndDrawing();
   }
